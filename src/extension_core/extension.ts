d793da9950bcfbef40b8fcc986d490be59143c2a
Andrew Nolte <24322714+AndrewNolte@users.noreply.github.com>
2026-09-03 14:53:24 -0400

    WCP: Add updated, tested implementation
    The existing wcp code was not wire compatible according to the spec. This one is tested against my branch of slang-server, which was tested against surfer.
    
    The 'uri' extension was removed, and a wcp session per viewer window is now used. The conneciton is established via a vscode command. The existing configs for address/port are still available to support the two mcp projects that seem to be using this.
    
    It looks like https://github.com/heyfey/sv-pathfinder uses the vaporview api and not wcp, despite the wcp impl seemingly being added for that use case.
    
    This uses generated bindings and types from the wcp schema; see https://gitlab.com/waveform-control-protocol/wcp/-/merge_requests/12
    
    PR: https://github.com/Lramseyer/vaporview/pull/208

--- src/extension_core/extension.ts
+++ src/extension_core/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 import { TimestampLinkProvider, NetlistLinkProvider } from './terminal_links';
 import { registerVaporviewCommands } from './commands';
 import { WaveformViewerProvider, VaporviewDocumentCollection } from './viewer_provider';
-import { updateWCPServerFromConfiguration, WCPServer } from './wcp_server';
+import { WCPServer } from './wcp_server';
 import type {
   VaporviewApi,
   OpenFileArgs,
@@ -45,23 +45,13 @@ export async function activate(context: vscode.ExtensionContext): Promise<Vaporv
   );
 
   // Initialize WCP Server
-  let wcpServer: WCPServer | null = null;
-  updateWCPServerFromConfiguration(wcpServer, viewerProvider, context);
-
-  // Store wcpServer reference for cleanup
-  context.subscriptions.push({
-    dispose: () => {
-      if (wcpServer) {
-        wcpServer.stop();
-        wcpServer = null;
-      }
-    }
-  });
+  const wcpServer = new WCPServer(viewerProvider, context);
+  context.subscriptions.push(wcpServer);
 
   // Listen for configuration changes
   context.subscriptions.push(vscode.workspace.onDidChangeConfiguration((e) => {
     if (e.affectsConfiguration('vaporview.wcp.enabled') || e.affectsConfiguration('vaporview.wcp.port')) {
-      updateWCPServerFromConfiguration(wcpServer, viewerProvider, context);
+      void wcpServer.updateConfiguration();
     }
 
     if (e.affectsConfiguration('workbench.colorTheme')) {
@@ -84,6 +74,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<Vaporv
 
   // Register commands (there are a lot of commands, so we register them in a separate file for cleanliness)
   registerVaporviewCommands(context, outputLog, viewerProvider, documentCollection, wcpServer);
+  await wcpServer.updateConfiguration();
 
   outputLog.appendLine('Vaporview Activated');
 
